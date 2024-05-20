de1fe687d386382972514711de47c2f23c082ca2
Jacobgijah <jacobgijjah@gmail.com>
2024-05-20 16:33:52 +0300

    adding compute columns

--- tax/admin.py
+++ tax/admin.py
@@ -3,16 +3,27 @@ from . import models
 
 @admin.register(models.Message)
 class MessageAdmin(admin.ModelAdmin):
-  list_display = ['message', 'message_alert', 'created_at', 'status']
-  list_editable = ['status']
+  list_display = ['message', 'message_alert', 'created_at', 'message_status']
   list_per_page = 10
+
+  @admin.display(ordering='status')
+  def message_status(self, message):
+    if message.status == True:
+      return 'Active'
+    return 'Inactive'
 
 
 
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
-  list_display = ['first_name', 'last_name', 'phone', 'email', 'organization_type', 'organization_name']
+  list_display = ['first_name', 'last_name', 'phone', 'email', 'organization_type', 'organization_name', 'customer_status']
   ordering = ['first_name', 'last_name']
   list_per_page = 10
+
+  @admin.display(ordering='status')
+  def customer_status(self, customer):
+    if customer.status == True:
+      return 'Active'
+    return 'Inactive'
 
 admin.site.register(models.TaxRegion)
