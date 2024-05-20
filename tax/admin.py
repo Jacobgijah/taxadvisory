ed21b7eca6128d3ba05cc82b80e6fb2fdb512fde
Jacobgijah <jacobgijjah@gmail.com>
2024-05-20 17:03:18 +0300

    messages count overriding queryset

--- tax/admin.py
+++ tax/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.db.models import Count
 from . import models
 
 @admin.register(models.Message)
@@ -16,9 +17,20 @@ class MessageAdmin(admin.ModelAdmin):
 
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
-  list_display = ['first_name', 'last_name', 'phone', 'email', 'organization_type', 'organization_name', 'customer_status']
+  list_display = ['first_name', 'last_name', 'phone', 'organization_type',
+                  'organization_name', 'customer_status', 'messages_count']
+  
   ordering = ['first_name', 'last_name']
   list_per_page = 10
+
+  @admin.display(ordering='messages_count')
+  def messages_count(self, customer):
+    return customer.messages_count
+  
+  def get_queryset(self, request):
+      return super().get_queryset(request).annotate(
+        messages_count=Count('message')
+      )
 
   @admin.display(ordering='status')
   def customer_status(self, customer):
