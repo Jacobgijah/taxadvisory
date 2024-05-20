ab37cbc5d74f62de7ca5c6629a5f6a551eee9408
Jacobgijah <jacobgijjah@gmail.com>
2024-05-20 16:22:36 +0300

    customizing list page

--- tax/admin.py
+++ tax/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
 from . import models
 
+@admin.register(models.Message)
+class MessageAdmin(admin.ModelAdmin):
+  list_display = ['message', 'message_alert', 'created_at', 'status']
+  list_editable = ['status']
+  list_per_page = 10
 
-admin.site.register(models.Customer)
-admin.site.register(models.Message)
+
+
+@admin.register(models.Customer)
+class CustomerAdmin(admin.ModelAdmin):
+  list_display = ['first_name', 'last_name', 'phone', 'email', 'organization_type', 'organization_name']
+  ordering = ['first_name', 'last_name']
+  list_per_page = 10
+
 admin.site.register(models.TaxRegion)
