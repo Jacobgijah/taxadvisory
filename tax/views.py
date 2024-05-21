d27e60b92b29cbbaf1f1b7e228bef8fbea56b9dd
Jacobgijah <jacobgijjah@gmail.com>
2024-05-21 20:05:40 +0300

    serializing message object

--- tax/views.py
+++ tax/views.py
@@ -1,13 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import Message
+from .serializers import MessageSerializer
 
 
 @api_view()
 def message_list(request):
-  return Response('ok')
+  queryset = Message.objects.all()
+  serializer = MessageSerializer(queryset, many=True)
+  return Response(serializer.data)
 
 
 @api_view()
 def message_detail(request, id):
-  return Response(id)
+  message = get_object_or_404(Message, pk=id)
+  serializer = MessageSerializer(message) # dictionary of message
+  return Response(serializer.data)
