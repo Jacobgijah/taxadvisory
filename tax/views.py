127856d636b5507df4222cfdbba3a89862eda487
Jacobgijah <jacobgijjah@gmail.com>
2024-05-23 18:18:25 +0300

    add ordering filter messages

--- tax/views.py
+++ tax/views.py
@@ -1,7 +1,7 @@
 from django.db.models.aggregates import Count
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import SearchFilter
+from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework import status
@@ -13,9 +13,10 @@ from .serializers import MessageSerializer, CustomerSerializer, TaxRegionSeriali
 class MessageViewSet(ModelViewSet):
   queryset = Message.objects.all()
   serializer_class = MessageSerializer
-  filter_backends = [DjangoFilterBackend, SearchFilter]
+  filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
   filterset_class = MessageFilter
   search_fields = ['message_alert']
+  ordering_fields = ['created_at']
 
   def get_serializer_context(self):
      return {'request': self.request}
