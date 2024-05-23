c99122513679d9a1d932a1ccf24320f07151a3fa
Jacobgijah <jacobgijjah@gmail.com>
2024-05-23 18:08:04 +0300

    add search function messages

--- tax/views.py
+++ tax/views.py
@@ -1,6 +1,7 @@
 from django.db.models.aggregates import Count
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework import status
@@ -12,8 +13,9 @@ from .serializers import MessageSerializer, CustomerSerializer, TaxRegionSeriali
 class MessageViewSet(ModelViewSet):
   queryset = Message.objects.all()
   serializer_class = MessageSerializer
-  filter_backends = [DjangoFilterBackend]
+  filter_backends = [DjangoFilterBackend, SearchFilter]
   filterset_class = MessageFilter
+  search_fields = ['message_alert']
 
   def get_serializer_context(self):
      return {'request': self.request}
